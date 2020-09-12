5a1d72d5278516d39f077ccfc2cfd0dd0b8e9a92
Peter Pfeufer <info@ppfeufer.de>
2020-09-13 01:57:37 +0200

    template tags no longer needed

--- discordpingformatter/templatetags/update_variable.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-@register.simple_tag
-def update_variable(value):
-    """Allows to update existing variable in template"""
-    return value
